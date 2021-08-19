6fa4e50bfac6dfd8f75d4b23bb7c204fc33b8afb
dreamheart <leondreamheart@outlook.com>
2021-08-19 10:02:41 -0400

    Fixed problem with chache files -the issue was loading the style.css

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,25 +3,15 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/manifest.webmanifest",
-  "/style.css",
-
-  // Load Files
-
+  "/manifest.webmanifest"
 ];
 
 // install
 self.addEventListener("install", function (evt) {
   // pre cache image data
   evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-  );
-    
-  // pre cache all static assets
-  evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
-
   // tell the browser to activate this service worker immediately once it
   // has finished installing
   self.skipWaiting();
