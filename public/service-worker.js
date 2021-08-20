c891ea66346b38f30d814845bc1926ee99883760
Julio Placeres <leondreamheart@outlook.com>
2021-08-19 23:18:39 -0400

    added db.js file which contains logic to save to the index.db, and updated manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,10 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/manifest.webmanifest"
+  "/manifest.webmanifest",
+  "/styles.css",
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png"
 ];
 
 // install
